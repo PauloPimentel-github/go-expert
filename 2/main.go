a144ca1d674d97015f727461b87bcaae3f66b3a9
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-17 15:57:56 -0300

    Fundação - Declaração e Atribuição

---
+++ 2/main.go
@@ -0,0 +1,19 @@
+package main
+
+const a = "Hello, World"
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "PH"
+	e float64 = 1.2
+)
+
+func main() {
+	a := "X"
+	a = "XX"
+	println(a)
+}
+
+func x() {
+}
