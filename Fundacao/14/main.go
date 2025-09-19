c9f11fe48a10f5ab7c7ef3c1c071cb004988e03a
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-18 22:24:47 -0300

    Fundação - Interfaces

---
+++ Fundacao/14/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
+type Pessoa interface {
+	Desativar()
+}
+
+type Empresa struct {
+	Nome string
+}
+
+func (e Empresa) Desativar() {
+
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereco
+}
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativado\n", c.Nome)
+}
+
+func Desativacao(pessoa Pessoa) {
+	pessoa.Desativar()
+}
+
+func main() {
+
+	ph := Cliente{
+		Nome:  "PH",
+		Idade: 30,
+		Ativo: true,
+	}
+	Desativacao(ph)
+	minhaEmpresa := Empresa{}
+	Desativacao(minhaEmpresa)
+}
