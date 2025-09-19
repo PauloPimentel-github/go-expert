87a8c7b0233fd4ab80672b34a97148a5b4abb6d9
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-18 22:13:11 -0300

    Fundação - Métodos em Structs

---
+++ Fundacao/13/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Endereco struct {
+	Logradouro string
+	Numero     int
+	Cidade     string
+	Estado     string
+}
+
+type Cliente struct {
+	Nome  string
+	Idade int
+	Ativo bool
+	Endereco
+}
+
+func (c Cliente) Desativar() {
+	c.Ativo = false
+	fmt.Printf("O cliente %s foi desativado\n", c.Nome)
+}
+
+func main() {
+
+	ph := Cliente{
+		Nome:  "PH",
+		Idade: 30,
+		Ativo: true,
+	}
+
+	ph.Desativar()
+}
