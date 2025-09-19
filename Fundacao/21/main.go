6ca69fdcb6dd61721afda90f99820383704dd277
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-19 14:17:19 -0300

    Fundação - Pacotes e módulos parte 3

--- Fundacao/21/main.go
+++ Fundacao/21/main.go
@@ -7,5 +7,13 @@ import (
 
 func main() {
 	s := matematica.Soma(10, 20)
+
 	fmt.Println("Resultado: ", s)
+	fmt.Println(matematica.A)
+
+	carro := matematica.Carro{Marca: "Ford"}
+	fmt.Println("Carro: ", carro.Marca)
+
+	carro2 := matematica.Carro{}
+	fmt.Println(carro2.Acelerar())
 }
