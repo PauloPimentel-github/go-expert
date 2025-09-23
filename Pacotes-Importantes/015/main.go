af16d2fc33a239f64f628bd5e3864041271d5932
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-23 11:21:25 -0300

    Pacotes Importantes - Templates com webserver

---
+++ Pacotes-Importantes/015/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Curso struct {
+	Name         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.New("template.html").ParseFiles("template.html"))
+		err := t.Execute(w, Cursos{
+			{Name: "Go Expert", CargaHoraria: 40},
+			{Name: "JavaScript Expert", CargaHoraria: 20},
+			{Name: "TypeScript Expert", CargaHoraria: 10},
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+	http.ListenAndServe(":8282", nil)
+}
