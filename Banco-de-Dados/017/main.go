8c06e34201014f431a70c29218f0963d432d1196
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-24 14:51:33 -0300

    Banco de Dados - Lock Otimista vs Pessimista

---
+++ Banco-de-Dados/017/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product `gorm:"many2many:product_categories;"`
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	Categories []Category `gorm:"many2many:product_categories;"`
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	tx := db.Begin()
+	var category Category
+	err = tx.Debug().Clauses(clause.Locking{Strength: "UPDATE"}).First(&category, 1).Error
+	if err != nil {
+		tx.Rollback()
+		panic(err)
+	}
+	category.Name = "Eletrônicos"
+	tx.Debug().Save(&category)
+	tx.Commit()
+}
