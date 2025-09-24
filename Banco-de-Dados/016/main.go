aa7c258b458b3a5019f61a0514941a04f7a0e7af
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-24 14:30:19 -0300

    Banco de Dados - Pegadinha no Many to Many

---
+++ Banco-de-Dados/016/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID       int `gorm:"primaryKey"`
+	Name     string
+	Products []Product `gorm:"many2many:product_categories;"`
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	Categories []Category `gorm:"many2many:product_categories;"`
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{}, &Category{})
+
+	// create category
+	category := Category{Name: "Esporte"}
+	db.Create(&category)
+
+	category2 := Category{Name: "Eletrônicos"}
+	db.Create(&category2)
+
+	// create product with category
+	db.Create(&Product{
+		Name:       "Chuteira Nike",
+		Price:      250.00,
+		Categories: []Category{category, category2},
+	})
+
+	var categories []Category
+	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
+	if err != nil {
+		panic(err)
+	}
+	for _, category := range categories {
+		println("Categoria:", category.Name)
+		for _, product := range category.Products {
+			println(" - Produto:", product.Name)
+		}
+	}
+}
