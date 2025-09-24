8173cd034a67df26b0ff87a69b3e6cfb5dab203c
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-24 11:47:14 -0300

    Banco de Dados - Has One

---
+++ Banco-de-Dados/012/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+	gorm.Model
+}
+
+type Product struct {
+	ID         int `gorm:"primaryKey"`
+	Name       string
+	Price      float64
+	CategoryID int
+	Category   Category
+	gorm.Model
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+
+	// create category
+	// category := Category{Name: "Eletrônicos"}
+	// db.Create(&category)
+
+	// create product with category
+	// product := Product{
+	// 	Name:       "Notebook",
+	// 	Price:      5000,
+	// 	CategoryID: category.ID,
+	// }
+	// db.Create(&product)
+
+	// relationship 1:N
+	var products []Product
+	db.Preload("Category").Find(&products)
+	for _, product := range products {
+		println(product.Name, product.Category.Name)
+	}
+}
