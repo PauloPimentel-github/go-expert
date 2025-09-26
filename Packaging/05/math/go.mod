d77d43edbd64a597a6f40c7e6845882a8fb77708
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-26 14:52:30 -0300

    Packaging - Trabalando com go mod replace

---
+++ Packaging/05/math/go.mod
@@ -0,0 +1,3 @@
+module github.com/PauloPimentel-github/go-expert/Packaging/03/math
+
+go 1.25.1
---
+++ Packaging/05/sistema/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/PauloPimentel-github/go-expert/Packaging/03/math"
+
+func main() {
+	m := math.NewMath(1, 2)
+	println(m.Add())
+}
