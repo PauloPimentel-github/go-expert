aecbae49e719edfbe8b04e8fa5d1e88c615870be
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-09-26 13:25:59 -0300

    Packaging - Acessando pacotes criados

---
+++ Packaging/02/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/PauloPimentel-github/go-expert/02/math"
+)
+
+func main() {
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
+	// fmt.Println("Hello, world!")
+}
