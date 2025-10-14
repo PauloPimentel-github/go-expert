d7170eac1b543ac5e3a8c738e41583ea2e191341
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-10-13 21:37:10 -0300

    Multithreading - Entendendo Mutex e Operações Atômicas

--- Multithreading/003/main.go
+++ Multithreading/003/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 	"time"
 )
 
 var number uint64 = 0
 
 func main() {
+	// m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		number++
+		//m.Lock()
+		atomic.AddUint64(&number, 1)
+		//m.Unlock()
 		time.Sleep(300 * time.Millisecond)
 		w.Write([]byte(fmt.Sprintf("Hello, World! Você é o visitante número: %d", number)))
 	})
