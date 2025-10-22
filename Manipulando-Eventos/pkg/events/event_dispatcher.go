fbdcd4bc3719ce4ad90cdf775de98425363a0855
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2025-10-21 23:50:49 -0300

    Manipulando-Eventos - Utilizando go routines no Dispatcher

--- Manipulando-Eventos/pkg/events/event_dispatcher.go
+++ Manipulando-Eventos/pkg/events/event_dispatcher.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
@@ -18,9 +19,12 @@ func NewEventDispatcher() *EventDispatcher {
 
 func (ev *EventDispatcher) Dispatch(event EventInterface) error {
 	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		wg := &sync.WaitGroup{}
 		for _, handler := range handlers {
-			handler.Handle(event)
+			wg.Add(1)
+			go handler.Handle(event, wg)
 		}
+		wg.Wait()
 	}
 	return nil
 }
